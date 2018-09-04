f32b382042551b5282758ea086b807f60a4009c4
= <=>
2018-09-04 20:09:46 -0300

    Aula JPA 2

--- src/fvs/edu/dominio/Aluno.java
+++ src/fvs/edu/dominio/Aluno.java
@@ -13,14 +13,18 @@ public class Aluno implements Serializable{
 	@Id
 	@GeneratedValue
 	private Integer id;
-	private Integer nome;
-	private Integer cpf;
-	private Integer rg;
+	private String nome;
+	private String cpf;
+	private String rg;
 	private Integer matricula;
 	
+	public Aluno() {
 	
+	}
 	
-	public Aluno(Integer id, Integer nome, Integer cpf, Integer rg, Integer matricula) {
+
+
+	public Aluno(Integer id, String nome, String cpf, String rg, Integer matricula) {
 		super();
 		this.id = id;
 		this.nome = nome;
@@ -28,17 +32,19 @@ public class Aluno implements Serializable{
 		this.rg = rg;
 		this.matricula = matricula;
 	}
-	
-	
-	@Override
-	public String toString() {
-		return "Aluno [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", matricula=" + matricula + "]";
-	}
-	
+
+
+
+
+
 
 	public Integer getId() {
 		return id;
 	}
+
+
+
+
 
 
 	public void setId(Integer id) {
@@ -46,34 +52,62 @@ public class Aluno implements Serializable{
 	}
 
 
-	public Integer getNome() {
+
+
+
+
+	public String getNome() {
 		return nome;
 	}
 
 
-	public void setNome(Integer nome) {
+
+
+
+
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
 
-	public Integer getCpf() {
+
+
+
+
+	public String getCpf() {
 		return cpf;
 	}
 
 
-	public void setCpf(Integer cpf) {
+
+
+
+
+	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
 
 
-	public Integer getRg() {
+
+
+
+
+	public String getRg() {
 		return rg;
 	}
 
 
-	public void setRg(Integer rg) {
+
+
+
+
+	public void setRg(String rg) {
 		this.rg = rg;
 	}
+
+
+
+
 
 
 	public Integer getMatricula() {
@@ -81,22 +115,42 @@ public class Aluno implements Serializable{
 	}
 
 
+
+
+
+
 	public void setMatricula(Integer matricula) {
 		this.matricula = matricula;
 	}
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Aluno [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", matricula=" + matricula + "]";
+	}
+
+
+
+
 
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
 		return result;
 	}
+
+
+
+
+
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -106,34 +160,15 @@ public class Aluno implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Aluno other = (Aluno) obj;
-		if (cpf == null) {
-			if (other.cpf != null)
-				return false;
-		} else if (!cpf.equals(other.cpf))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (matricula == null) {
-			if (other.matricula != null)
-				return false;
-		} else if (!matricula.equals(other.matricula))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		if (rg == null) {
-			if (other.rg != null)
-				return false;
-		} else if (!rg.equals(other.rg))
-			return false;
 		return true;
 	}
 	
+
 	
 	
 }
