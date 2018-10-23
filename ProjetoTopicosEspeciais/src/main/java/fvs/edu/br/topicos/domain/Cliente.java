3f006d29b4d551565b006b6bd53b0c8a705ffc0b
ANA <apico101010>
2018-10-23 21:03:07 -0200

    Implementaçao do relacionamento entre pedido e produto

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Cliente.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Cliente.java
@@ -18,39 +18,40 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import fvs.edu.br.topicos.enums.TipoCliente;
 
-@Entity
-public class Cliente implements Serializable {
-	private static final long serialVersionUID = 1L;
 
+@Entity
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
+	private String nome;
 	private String email;
 	private String cpfOuCnpj;
 	private TipoCliente tipo;
-    
-	@OneToMany(mappedBy="cliente")
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> endereco = new ArrayList<>();
-
+	
 	@ElementCollection
-	@CollectionTable(name = "TELEFONE")
+	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-    
+	
+	@OneToMany(mappedBy = "cliente")
 	@JsonIgnore
-	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-
+	
 	public Cliente() {
-
+		
 	}
 
 	
 
-	public Cliente(Integer id, String name, String email, String cpfOuCnpj, TipoCliente tipo) {
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = tipo;
@@ -88,12 +89,12 @@ public class Cliente implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getEmail() {
@@ -144,5 +145,7 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+	
+	
 
 }
--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Pedido.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Pedido.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -12,9 +13,9 @@ import javax.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-public class Pedido implements Serializable {
+public class Pedido implements Serializable{
 	private static final long serialVersionUID = 1L;
-    
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
@@ -23,15 +24,13 @@ public class Pedido implements Serializable {
 	private Date instante;
 	
 	@ManyToOne
-	@JoinColumn(name="endereco_de_entrega_id")
-	private Endereco enderecoEntrega;
-	
-	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
 	
-	public Pedido() {
-		
-	}
 	
+	@ManyToOne
+	@JoinColumn(name="endereco_de_entrega_id")
+	private Endereco enderecoEntrega;
+	
+
 }
